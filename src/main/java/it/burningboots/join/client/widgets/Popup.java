95d17fcf26366b7afc79c25d411f759957236bcb
Paolo Tacconi <p.tacconi@giunti.it>
2016-02-18 12:15:56 +0100

    Ok button on popups

--- src/main/java/it/burningboots/join/client/widgets/Popup.java
+++ src/main/java/it/burningboots/join/client/widgets/Popup.java
@@ -4,13 +4,17 @@ import it.burningboots.join.client.ClientConstants;
 import it.burningboots.join.client.LocaleConstants;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Popup extends PopupPanel {
 	private LocaleConstants constants = GWT.create(LocaleConstants.class);
+	private Button button = null;
 	
 	public Popup(String message) {
 		// PopupPanel's constructor takes 'auto-hide' as its boolean parameter.
@@ -20,17 +24,35 @@ public class Popup extends PopupPanel {
 		// PopupPanel is a SimplePanel, so you have to set it's widget property
 		// to
 		// whatever you want its contents to be.
+		final PopupPanel pop = this;
 		VerticalPanel vp = new VerticalPanel();
 		vp.add(new HTML(ClientConstants.MSG_ICON_WARN+" <b>"+constants.warning()+"</b>"));
 		vp.add(new HTML(message));
+		button = new Button("&nbsp;OK&nbsp;");
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				pop.hide();
+			}
+		});
+		vp.add(button);
 		setWidget(vp);
 	}
 
 	public Popup(Throwable e) {
 		super(true);
+		final PopupPanel pop = this;
 		VerticalPanel vp = new VerticalPanel();
 		vp.add(new HTML(ClientConstants.MSG_ICON_ERROR+" <b>"+constants.error()+"</b>"));
 		vp.add(new HTML(e.getMessage()));
+		button = new Button("&nbsp;OK&nbsp;");
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				pop.hide();
+			}
+		});
+		vp.add(button);
 		setWidget(vp);
 	}
 
